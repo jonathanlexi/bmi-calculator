c4746b847619ec6055eb6e4b2945e56f435bbe90
jonathanlexi <jonathanlexi02@gmail.com>
2022-10-07 22:43:37 +0700

    mengubah fitur

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
+let button = document.getElementById('btn')
 
-function bmicalculate() {
+button.addEventListener('click', (e) => {
 
     let height = document.getElementById('height').value
     let weight = document.getElementById('weight').value
@@ -16,12 +17,31 @@ function bmicalculate() {
         document.getElementById('output').innerHTML = `Your BMI is ${bmi} which means You are Normal`
     }else if(bmi>25 && bmi<29.9){
         document.getElementById('output').innerHTML = `Your BMI is ${bmi} which means You are Overweight`
-    }else{
+    }else if (bmi>30){
         document.getElementById('output').innerHTML = `Your BMI is ${bmi} which means You are Obesity`
+    } else{
+        document.getElementById('output').innerHTML = " "
     }
+
+
+
+    if (weight == " " || isNaN(weight) || weight<=0){
+        document.getElementById('weight_error').innerHTML = 'Input the number please'
+    }else{
+        document.getElementById('weight_error').innerHTML = ' '
+    }
+
+
+    if (height == " " || isNaN(height) || height<=0){
+        document.getElementById('height_error').innerHTML = 'Input the number please'
+    }else{
+        document.getElementById('height_error').innerHTML = ' '
+    }
+
+    e.preventDefault()
     
    
-}
+});
 
 
 
